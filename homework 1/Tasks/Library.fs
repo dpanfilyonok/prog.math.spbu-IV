ec16a300cef58ae0cec0ce1d545bbc1ea58750e7
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-02-21 02:33:56 +0300

    Add factorial (but its not working)

---
+++ homework 1/Tasks/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace Tasks
+
+module Combinators = 
+    let iif condition x y = if condition then x else y
+    let isZero = (=) 0
+    let c f x y = f y x
+    let pred = c (-) 1
+    let rec y f = f (y f)
+
+module Factorial =
+    open Combinators    
+    let rec factorial1 n =  iif (isZero n) 1 ((*) n (factorial1 (n - 1)))
+    let rec factorial2 n = (fun f -> iif (isZero n) 1 ((*) n (f <| pred n))) factorial2
+    let factorial3 = y (fun f -> fun n -> iif (isZero n) 1 ((*) n (f <| pred n)))
