ca8f99fa2e99b7563a8f05f338cc1b2cada9ab79
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-03-16 15:25:20 +0300

    Add task1 partly solution

---
+++ homework 3/src/Tasks/CountOfEven.fs
@@ -0,0 +1,11 @@
+﻿module CountOfEven
+
+let isEven x =  x % 2 = 0
+
+let usingFilter list =
+    list
+    |> List.filter isEven
+
+let usingFold list =
+    list
+    |> List.fold (fun x acc -> if isEven then (acc + 1)) else acc) 0
