024cebfabe9e3bd6c8d62241b4508e9124d2e658
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-03-21 14:02:25 +0300

    Add task 1 and add hw3 to azure ci build

--- homework 3/src/Tasks/CountOfEven.fs
+++ homework 3/src/Tasks/CountOfEven.fs
@@ -1,11 +1,30 @@
 ﻿module CountOfEven
 
-let isEven x =  x % 2 = 0
+open System
 
+module StuffFuns = 
+    let isEven x =  x % 2 = 0
+    let mapSnd f (x, y) = x, f y
+    let uncurry f (x, y) = f x y
+    let curry f x y = f (x, y)
+    let c f x y = f y x
+    let succ = (+) 1
+    let mod2 = c (%) 2
+
+open StuffFuns
+
+/// Count of even in list using filter fun
 let usingFilter list =
     list
     |> List.filter isEven
 
+/// Count of even in list using map fun
+let usingMap list =
+    list
+    |> List.map (succ >> mod2 >> Math.Abs)
+    |> List.sum 
+
+/// Count of even in list using fold fun
 let usingFold list =
     list
-    |> List.fold (fun x acc -> if isEven then (acc + 1)) else acc) 0
+    |> List.fold (curry <| (mapSnd (succ >> mod2 >> Math.Abs) >> uncurry (+))) 0
---
+++ homework 3/test/Tasks.Test/CountOfEvenTest.fs
@@ -0,0 +1,53 @@
+namespace Tests
+
+open NUnit.Framework
+open FsUnit
+
+open CountOfEven
+
+[<TestFixture>]
+type CountOfEvenTest () =
+
+    let testCases = [
+        // empty list
+        [], 0
+
+        // list with only zero which is even
+        [0], 1
+
+        // list with only one which is odd
+        [1], 0
+
+        // list with only minus two which even 
+        [-2], 0
+
+        // list with only two which even 
+        [2], 1
+
+        // list with same count of even and odd nums
+        [1;2;3;4;5;6], 3
+
+        // list with same count of negative even and odd nums
+        [-1;-2;-3;-4;-5;-6], 3
+
+        // list with only odd nums
+        [1;-3;5], 0
+
+        // list in which even > odd nums
+        [0;1;2;3;4;5;6], 4 
+    ]
+
+    [<Test>]
+    [<TestCaseSource("testCases")>]
+    member this.``usingFilter should work properly`` list expectedValue =
+        usingFilter list |> should equal expectedValue 
+
+    [<Test>]
+    [<TestCaseSource("testCases")>]
+    member this.``usingMap should work properly`` list expectedValue =
+        usingMap list |> should equal expectedValue 
+
+    [<Test>]
+    [<TestCaseSource("testCases")>]
+    member this.``usingFold should work properly`` list expectedValue =
+        usingFold list |> should equal expectedValue 
