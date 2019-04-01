c03024eebe6cbf8c72b2720552a3275fe4682725
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-04-01 13:32:50 +0300

    Add some rough draft

---
+++ homework 4/Lambda Calculus Interpreter/Interpreter/Library.fs
@@ -0,0 +1,24 @@
+﻿namespace LambdaCalculus 
+
+module Interpreter = 
+
+    type VarType = char
+
+    type LambdaTerm = 
+        | Variable of VarType
+        | Application of LambdaTerm * LambdaTerm
+        | Abstraction of VarType * LambdaTerm
+    
+    let applyBetaConversion (abstr: LambdaTerm, term: LambdaTerm) = ()
+        
+
+    let rec reductToNormalForm (lambdaTerm: LambdaTerm) = 
+        match lambdaTerm with
+        | Variable x -> Variable x
+        | Abstraction(var, term) -> Abstraction(var, reductToNormalForm term)
+        | Application(l, r) -> 
+            if l :? Abstraction then applyBetaConversion l r
+            else Application(reductToNormalForm l, reductToNormalForm r)
+
+
+    
