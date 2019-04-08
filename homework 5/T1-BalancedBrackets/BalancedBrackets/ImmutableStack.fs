e93e76eae6d8accf43f431b255badad8de1b47d5
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-04-08 22:05:15 +0300

    Add immutable stack

---
+++ homework 5/T1-BalancedBrackets/BalancedBrackets/ImmutableStack.fs
@@ -0,0 +1,18 @@
+namespace BalancedBrackets
+
+type 'a ImmutableStack =
+    | Empty 
+    | Stack of 'a * 'a ImmutableStack
+
+    member this.Push x = 
+        Stack(x, this)
+
+    member this.Pop () = 
+        match this with
+        | Empty -> failwith "Underflow"
+        | Stack(top, _) -> top
+
+    member this.Top () = 
+        match this with
+        | Empty -> failwith "Contain no elements"
+        | Stack(_, stack) -> stack
---
+++ homework 5/T1-BalancedBrackets/BalancedBrackets/Library.fs
@@ -0,0 +1,23 @@
+﻿namespace BalancedBrackets
+
+module BalanceChecker =    
+    let c f x y = f y x
+
+    let openBrackets = ['('; '{'; '['] 
+    let closeBrackets = [')'; '}'; ']']
+
+    let pairBrackets = List.zip openBrackets closeBrackets |> Map.ofList
+
+    let parseSeq (sequence: string, stack: char ImmutableStack) = 
+        5
+
+    // let stack = Stack<char>()
+
+    // let rec parseSeq (sequence, stack: char Stack)= 
+    //     match sequence with
+    //     | ch :: tail when ch |> c List.contains openBrackets -> 
+    //         ch |> stack.Push
+    //         parseSeq tail stack
+    //     | ch :: tail when ch |> c List.contains openBrackets
+
+    // let checkIfBalanced (sequence: #seq<char>) = 
