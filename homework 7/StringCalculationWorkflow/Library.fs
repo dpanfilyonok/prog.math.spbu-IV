f3f6c910ed23a2e59106c78d23d95e61c555e82a
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-04-09 19:30:24 +0300

    Add string calulation workflow

---
+++ homework 7/StringCalculationWorkflow/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace StringCalculationWorkflow
+
+module StringCalculationWorkflow =
+    open System
+    type CalculateBuilder() = 
+        member this.Bind(x, f) = 
+            match Int32.TryParse x with
+            | true, num -> f num
+            | _ -> None // тут мы сразу возвращаем None
+        member this.Return(x) = 
+            Some x
+
+    let calculate = CalculateBuilder()
+
+    let res () = calculate {
+        let! x = "1"
+        let! y = "2"
+        let z = x + y
+        return z
+    }
