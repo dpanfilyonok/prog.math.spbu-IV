a4491524aaf009c591c2a431caa0fee01d69ed23
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-04-14 20:43:10 +0300

    Move a-conversion and substitution funcs to dunion

--- homework 4/Lambda Calculus Interpreter/Interpreter/Library.fs
+++ homework 4/Lambda Calculus Interpreter/Interpreter/Library.fs
@@ -14,45 +14,46 @@ module Interpreter =
             | Variable x -> Set.ofList [x]
             | Application(left, right) -> 
                 (left.FreeVariables |> Set.union) <| right.FreeVariables
-            | Abstraction(abstractionVar, abstractionTerm) -> 
-                (abstractionTerm.FreeVariables |> Set.difference) <| Set.ofList [abstractionVar] 
-        
-    let rec applyAlphaConversion (term: LambdaTerm) =
-        match term with
-        | Abstraction(abstractionVar, abstractionTerm) -> 
-            "`"
-            |> (+) abstractionVar
-            |> (fun x -> Abstraction(x, applySubstitution abstractionTerm abstractionVar (Variable x)))
-        | _ -> failwith "sss"
-    and applySubstitution (target: LambdaTerm) (var: VarType) (source: LambdaTerm) = 
-        match target with
-        | Variable x when x = var -> source
-        | Variable x when x <> var -> target
-        | Application(n, m) -> 
-            Application(applySubstitution n var source, applySubstitution m var source)
-        | Abstraction(abstractionVar, _) when abstractionVar = var -> target
-        | Abstraction(abstractionVar, _) when abstractionVar <> var ->
-            let newTarget = 
-                if source.FreeVariables |> Set.contains abstractionVar 
-                    then applyAlphaConversion target
-                else target 
-            match newTarget with
-            | Abstraction(abstractionVar, abstractionTerm) -> 
-                Abstraction(abstractionVar, applySubstitution abstractionTerm var source)
-            | _ -> failwith "sss"
-        | _ -> failwith "sss"
+            | Abstraction(abstractionVariable, abstractionTerm) -> 
+                (abstractionTerm.FreeVariables |> Set.difference) <| Set.ofList [abstractionVariable] 
 
-    let applyBetaConversion (abstr: LambdaTerm) (term: LambdaTerm) = 
-        match abstr with
-        | Abstraction(abstractionVar, abstractionTerm) -> 
-            applySubstitution abstractionTerm abstractionVar term
-        | _ -> failwith "sss"
+        member this.ApplyAlphaConversion () =
+            match this with
+            | Abstraction(abstractionVariable, abstractionTerm) -> 
+                "`"
+                |> (+) abstractionVariable
+                |> (fun x -> Abstraction(x, abstractionTerm.ApplySubstitution abstractionVariable (Variable x)))
+            | _ -> failwith "Term is not lambda abstraction, only them could be alpha reduced"
+            
+        member this.ApplySubstitution (targetVariable: VarType) (sourceTerm: LambdaTerm) = 
+            match this with
+            | Variable x when x = targetVariable -> sourceTerm
+            | Variable x when x <> targetVariable -> this
+            | Application(left, right) -> 
+                Application(left.ApplySubstitution targetVariable sourceTerm, right.ApplySubstitution targetVariable sourceTerm)
+            | Abstraction(abstractionVariable, _) when abstractionVariable = targetVariable -> this
+            | Abstraction(abstractionVariable, _) when abstractionVariable <> targetVariable ->
+                let alphaReducedTerm = 
+                    if sourceTerm.FreeVariables |> Set.contains abstractionVariable 
+                        then this.ApplyAlphaConversion ()
+                    else this 
+                match alphaReducedTerm with
+                | Abstraction(abstractionVariable, abstractionTerm) -> 
+                    Abstraction(abstractionVariable, abstractionTerm.ApplySubstitution targetVariable sourceTerm)
+                | _ -> failwith "Term after alpha conversion should be lambda abstraction"
+            | _ -> failwith "Something went wrong in term substitution"
+
+    let applyBetaConversion (abstraction: LambdaTerm) (term: LambdaTerm) = 
+        match abstraction with
+        | Abstraction(abstractionVariable, abstractionTerm) -> 
+            abstractionTerm.ApplySubstitution abstractionVariable term
+        | _ -> failwith "Term is not lambda abstraction, only them could be alpha reduced"
 
     let rec reductToNormalForm (term: LambdaTerm) = 
         match term with
         | Variable x -> Variable x
-        | Abstraction(abstractionVar, abstractionTerm) -> 
-            Abstraction(abstractionVar, reductToNormalForm abstractionTerm)
+        | Abstraction(abstractionVariable, abstractionTerm) -> 
+            Abstraction(abstractionVariable, reductToNormalForm abstractionTerm)
         | Application(left, right) -> 
             match left with
             | Abstraction _ -> applyBetaConversion left right
