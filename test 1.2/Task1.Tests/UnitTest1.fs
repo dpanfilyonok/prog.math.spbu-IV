d312c9d33bb9d6bf5db26f079fd434297d5ec8a9
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-04-26 18:40:32 +0300

    Add one more test to task 1 tests

--- test 1.2/Task1.Tests/UnitTest1.fs
+++ test 1.2/Task1.Tests/UnitTest1.fs
@@ -14,3 +14,7 @@ type TestClass () =
     [<Test>]
     member this.``Smoke test with first 6`` () =
         alternatingNumberSequence |> Seq.take 6 |> Seq.toList |> should equal [1; -2; 3; -4; 5; -6]
+
+    [<Test>]
+    member this.``Taking first 100_000 of seq should not fail`` () =
+        alternatingNumberSequence |> Seq.take 100_000 |> ignore
