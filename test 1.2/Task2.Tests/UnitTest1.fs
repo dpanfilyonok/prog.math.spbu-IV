8927a4ee48a616c684e6ceec5904f45cda53c68d
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-04-26 18:30:33 +0300

    Fix test in task 2

--- test 1.2/Task2.Tests/UnitTest1.fs
+++ test 1.2/Task2.Tests/UnitTest1.fs
@@ -9,4 +9,4 @@ type TestClass () =
 
     [<Test>]
     member this.``Max palindrome should be 906609`` () =
-        maxThreeDigitPalindrome () |> should equal 906609
+        getMaxThreeDigitPalindrome () |> should equal 906609
