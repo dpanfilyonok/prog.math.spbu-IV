5aba028e14c2fe890a9572f8543e10dc43485272
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2019-07-22 14:31:15 +0300

    Hw3 (#5)
    * Add task1 partly solution
    
    * Add task 1 and add hw3 to azure ci build
    
    * Add task 2, 4
    
    * Add tests for task 4
    
    * Add task 3
    
    * Add test for task 2
    
    * Add tests for task 3
    
    * Fix bugs but still one exists :(
    
    * Fix bug with division by zero test (now it pass)
    
    * Add more beautiful isPrime check

---
+++ homework 3/test/Tasks.Test/MapBinaryTreeTest.fs
@@ -0,0 +1,55 @@
+namespace Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Tasks.MapBinaryTree
+
+[<TestFixture>]
+type MapBinaryTreeTest () =
+
+    let c f x y = f y x
+
+    let rootTree = Tree(-5, Empty, Empty)
+    let balancedTree =  Tree(1, 
+                            Tree(2, 
+                                Tree(4, Empty, Empty), 
+                                Tree(5, Empty, Empty)), 
+                            Tree(3, 
+                                Tree(6, Empty, Empty), 
+                                Tree(7, Empty, Empty)))
+    let flatternTree = Tree(1, Tree(2, Tree(3, Tree(4, Tree(5, Empty, Empty), Empty), Empty), Empty), Empty)
+
+    [<Test>]
+    member this.``mapping rootTree with id should return same tree`` () =
+        rootTree |> mapBinaryTree id |> should equal rootTree
+
+    [<Test>]
+    member this.``mapping balancedTree with id should return same tree`` () =
+        balancedTree |> mapBinaryTree id |> should equal balancedTree
+
+    [<Test>]
+    member this.``mapping flatternTree with id should return same tree`` () =
+        flatternTree |> mapBinaryTree id |> should equal flatternTree
+
+
+    [<Test>]
+    member this.``mapping rootTree with pown2 should return correct tree`` () =
+        let expected = Tree(25, Empty, Empty)
+        rootTree |> mapBinaryTree (c pown 2) |> should equal expected
+
+    [<Test>]
+    member this.``mapping balancedTree with pown2 should return correct tree`` () =
+        let expected = Tree(1, 
+                            Tree(4, 
+                                Tree(16, Empty, Empty), 
+                                Tree(25, Empty, Empty)), 
+                            Tree(9, 
+                                Tree(36, Empty, Empty), 
+                                Tree(49, Empty, Empty)))
+        balancedTree |> mapBinaryTree (c pown 2) |> should equal expected
+
+    [<Test>]
+    member this.``mapping flatternTree with pown2 should return correct tree`` () =
+        let expected = Tree(1, Tree(4, Tree(9, Tree(16, Tree(25, Empty, Empty), Empty), Empty), Empty), Empty)
+        flatternTree |> mapBinaryTree (c pown 2) |> should equal expected
