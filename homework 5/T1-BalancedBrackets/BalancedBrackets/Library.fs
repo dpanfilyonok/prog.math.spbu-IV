f4aeac2e047c97144e8a6fc2bd4aa36909d50d5d
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-07-23 00:46:26 +0300

    Add task 1 algo

--- homework 5/T1-BalancedBrackets/BalancedBrackets/Library.fs
+++ homework 5/T1-BalancedBrackets/BalancedBrackets/Library.fs
@@ -1,18 +1,28 @@
 ﻿namespace BalancedBrackets
 
-module BalanceChecker =    
-    let c f x y = f y x
+module BalanceChecker =  
+    open FSharpx.Collections
 
     let openBrackets = ['('; '{'; '['] 
     let closeBrackets = [')'; '}'; ']']
-
     let pairBrackets = List.zip openBrackets closeBrackets |> Map.ofList
 
-    let filterBrackets str = 
-        str
-        |> Seq.filter (fun x -> (openBrackets |> List.contains x || closeBrackets |> List.contains x))
+    let (|Open|Closed|) char = 
+        if openBrackets |> List.contains char
+            then Open
+        else Closed
 
-    let rec parseSeq (sequence: char seq, stack: char ImmutableStack) = 
-        match sequence with 
-        | 
-
+    let isBracketSequenceCorrect (sequence: char seq) = 
+        let rec loop (lazyList: char LazyList, stack: char ImmutableStack, isIncorrect: bool) = 
+            match lazyList with
+            | LazyList.Nil ->
+                // true if seq is correct
+                match stack with
+                | Empty -> not isIncorrect
+                | _ -> false
+            | LazyList.Cons(head, tail) ->
+                match head with
+                | Open -> loop (tail, stack.Push head, false)
+                | Closed when pairBrackets.[stack.Top ()] = head -> loop (tail, stack.Pop (), false)
+                | _ -> loop (LazyList.empty, Empty, true)
+        loop (LazyList.ofSeq sequence, Empty, false)
