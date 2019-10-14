dfe670bf882a2a90ab59b3f4b353f62c1435c4cb
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2019-10-14 18:50:50 +0300

    Another attempt to use sql provider - failure

--- homework 5/T3-Phonebook/Phonebook/Phonebook.fs
+++ homework 5/T3-Phonebook/Phonebook/Phonebook.fs
@@ -6,52 +6,45 @@ module Logic =
     exception RecordException of string
 
     [<Literal>]
-    let ConnectionString = @"
-        Server=tcp:dbspbu.database.windows.net,1433;
-        Initial Catalog=Phonebook;
-        Persist Security Info=False;
-        User ID=anticnvm;
-        Password=vbnz@vbnz1999;
-        MultipleActiveResultSets=False;
-        Encrypt=True;
-        TrustServerCertificate=False;
-        Connection Timeout=30;"
+    let ConnectionString = "Data Source=" + __SOURCE_DIRECTORY__  + @"/Phonebook.db;Version=3;"
 
+    [<Literal>]
+    let ResolutionStr = 
+        @"/home/anticnvm/.nuget/packages/system.data.sqlite.core/1.0.111/lib/netstandard2.0"
+        
     type SqlConnection = SqlDataProvider<
-                            Common.DatabaseProviderTypes.MSSQLSERVER, 
-                            ConnectionString, 
-                            UseOptionTypes = true>
+                            Common.DatabaseProviderTypes.SQLITE, 
+                            ConnectionString = ConnectionString,
+                            ResolutionPath = ResolutionStr,
+                            SQLiteLibrary = Common.SQLiteLibrary.SystemDataSQLite>
 
     let ctx = SqlConnection.GetDataContext()
 
     let getAllRecords () = 
         query {
-            for row in ctx.Dbo.Phonebook do
+            for row in ctx.Main.Phonebook do
                 select (row.FullName, row.PhoneNumber)
         } |> Seq.toList
     
     let addRecord (name: string, number: string) = 
         try 
-            let newRow = ctx.Dbo.Phonebook.``Create(FullName, PhoneNumber)``(name, number)
+            let newRow = ctx.Main.Phonebook.``Create(FullName, PhoneNumber)``(name, number)
             ctx.SubmitUpdates()
         with 
         | :? System.Data.SqlClient.SqlException as e 
-            when e.Message.Contains "Violation of UNIQUE KEY constraint 'UQ_Record'" 
-            -> raise <| RecordException "FAIL: Given record has already exist!"
-        | :? System.Data.SqlClient.SqlException as e 
-            when e.Message.Contains "The INSERT statement conflicted with the CHECK constraint \"CK__Phonebook__Phone__49C3F6B7\""
-            -> raise <| RecordException "FAIL: Given record has a phone number consisting not only of numbers"
+            when e.Message.Contains "UNIQUE constraint failed" 
+            -> raise <| RecordException "FAIL: Given phone number has already exist!"
 
     let getPhonesByName name = 
         query {
-            for record in ctx.Dbo.Phonebook do
+            for record in ctx.Main.Phonebook do
                 where (record.FullName = name)
                 select record.PhoneNumber
         } |> Seq.toList
      
     let getNameByPhone number = 
         query {
-            for record in ctx.Dbo.Phonebook do
+            for record in ctx.Main.Phonebook do
                 where (record.PhoneNumber = number)
                 select record.FullName
         } |> Seq.toList
